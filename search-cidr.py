e85cc4a552f60946132c206ccfd0f05678f49bc7
hwalker145 <hwalker145@gmail.com>
2024-01-16 13:21:48 -0500

    added dataframe-based reporting of the results

--- search-cidr.py
+++ search-cidr.py
@@ -2,6 +2,7 @@ from netaddr import IPNetwork, all_matching_cidrs
 from openpyxl import load_workbook, Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 from pathlib import Path
+import pandas as pd
 import sys
 
 # accepted header names
@@ -14,12 +15,12 @@ headers = [
 
 # checks if there is the correct amount of arguments on the CLI
 if len(sys.argv) < 3:
-    print("USAGE: \nbook.xlsx address")
+    print("USAGE: \n<file> <address>")
     exit()
 
 # checks the xlsx file in the CLI if it is really a .xlsx
 if Path(sys.argv[1]).suffix != ".xlsx":
-    print("USAGE: \nbook.xlsx address")
+    print("USAGE: \n<file> <address>")
     exit()
 
 # this function checks if the IP ranges are valid before constructing as IPNetwork()
@@ -47,24 +48,44 @@ def readSheet(sheet: Worksheet, ranges: list[IPNetwork]):
             if cell.value:
                 # I call the stringtorange function to make sure each address
                 # is a proper cidr address/range
-                ranges.append(stringToRange(cell.value))
+                ranges.append({'index': cell.row, 'cidr': stringToRange(cell.value)})
     # returns final product
     return ranges
+
+# this function compiles the matches and turns it into a df
+def reportMatches(sheet: Worksheet, matches: list[dict]) -> None:
+    # grabs the header values as values
+    data = [[cell.value for cell in ws[1]]]
+
+    # appends all the data into a list
+    for m in matches:
+        data.append([cell.value for cell in ws[m['index']]])
+
+    # turns it into a df
+    df = pd.DataFrame(data[1:], columns = data[0])
+
+    # TODO is to figure out how much of the df to print
+    # for now it's the whole thing
+    print(df)
 
 address = sys.argv[2]
 
 wb: Workbook = load_workbook(sys.argv[1])
 
-ranges: list[IPNetwork] = []
+ranges: list[dict] = []
 
 # calling the readSheet subroutine
 for ws in wb.worksheets:
     readSheet(ws, ranges)
 
 # calls the all_matching_cidrs function from netaddr
-matches: list[IPNetwork] = all_matching_cidrs(address, ranges)
+matches: list[IPNetwork] = []
+
+# iterates through ranges to make matches
+for range in ranges:
+    if address in range['cidr']:
+        matches.append(range)
 
 # reports
 print(str(len(matches)) + " matching cidr\nfor the given address.\n")
-for match in matches:
-    print(match.cidr)
+reportMatches(ws, matches)
